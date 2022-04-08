030cdd9e8315afcfd2de63c4f6864c0b30f5b999
Abubow <sabuzar00@gmail.com>
2022-04-08 16:24:02 +0500

    added LinkedListADT

---
+++ C++/Basic_Concepts/Data_Structures/LinkedList/LinkedListADT.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+template <typename type>
+class LinkedListADT{
+public:
+    virtual void insert(type) = 0; 
+    virtual void insert(type, int) = 0; // O(n)
+    virtual void insertAtStart(type) = 0; // O(1)
+    virtual void insertAtEnd(type) = 0; // O(1)
+    virtual void remove(int) = 0;
+    virtual void remove(type) = 0;
+    virtual void removeAtStart() = 0; // O(1)
+    virtual void removeAtEnd() = 0; // O(1)
+    virtual void get(int) = 0; 
+    virtual void print() = 0; // O(n)
+    virtual ~LinkedListADT(){}
+};
