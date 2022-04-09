09565356de0d81f91e04effcbb311bdba241a353
Abubow <sabuzar00@gmail.com>
2022-04-09 11:13:26 +0500

    provided time complexity of each function

--- C++/Basic_Concepts/Data_Structures/Stack/StackADT.h
+++ C++/Basic_Concepts/Data_Structures/Stack/StackADT.h
@@ -2,10 +2,10 @@
 template <typename type>
 class StackADT{
 public:
-    virtual void push(type) = 0;
-    virtual type pop() = 0;
-    virtual bool isEmpty() = 0;
-    virtual type top() = 0;
-    virtual ~StackADT(){}
-    virtual void print() = 0;
+    virtual void push(type) = 0; // O(1)
+    virtual type pop() = 0; // O(1)
+    virtual bool isEmpty() = 0; // O(1)
+    virtual type top() = 0; // O(1)
+    virtual ~StackADT(){} // O(1)
+    virtual void print() = 0; // O(n)
 };
